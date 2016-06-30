577621a7114097bdbd4256e2fe6ab602c91c1df6
Felipe Barreto Volpone <felipevolpone@gmail.com>
2016-06-30 09:50:28 -0300

    Parsing the method name and generating a dict to represent the query

--- active_peewee/active_peewee.py
+++ active_peewee/active_peewee.py
@@ -1,16 +1,39 @@
+import peewee
 
-class ActiveMeta(type):
+
+class __ParseMeta(type):
 
     def __getattr__(self, method_name):
+        """here is where the magic happens."""
+
         def inner(*args, **kwargs):
-            return method_name
+            return self.__parse(method_name, *args)
 
         inner.__name__ = method_name
         return inner
 
+    def __parse(self, method_name, *args):
+        fields_to_parse = method_name.replace('filter_by_', '').split('_')
 
-class ActivePeewee(object):
-    __metaclass__ = ActiveMeta
+        fd, op = [], []
+        i = 0
+        for part in fields_to_parse:
+            if i % 2 == 0:  # eh field
+                fd.append(part)
+            else:  # eh operador
+                op.append(part)
 
-    # the idea is Peewee models inhenrit this class to provide
-    # the activepeewee features to their models
+            i += 1
+
+        query = {}
+        if len(op) == 1:
+            query['agregate'] = op[0]
+
+        for idx, p in enumerate(args):
+            query[fd[idx]] = {'operator': 'eq', 'value': p}
+
+        return query
+
+
+class ActiveMeta(__ParseMeta, peewee.BaseModel):
+    pass
--- tests/test_orm.py
+++ tests/test_orm.py
@@ -1,9 +1,37 @@
 
-from active_peewee.active_peewee import ActivePeewee
+import peewee
+from active_peewee.active_peewee import ActiveMeta
 import unittest
+
+db = peewee.SqliteDatabase('database.db')
+
+
+class Person(peewee.Model):
+    __metaclass__ = ActiveMeta
+
+    name = peewee.CharField()
+    age = peewee.IntegerField()
+
+    class Meta:
+        database = db
 
 
 class TestActivePeeewee(unittest.TestCase):
 
-    def test(self):
-        self.assertEqual('bla', ActivePeewee.bla())
+    def setUp(self):
+        db.create_tables([Person])
+
+    def tearDown(self):
+        db.drop_tables([Person])
+
+    def test_parse(self):
+        print 'asdauidhsaui', type(Person)
+        query = Person.filter_by_name_and_age('felipe', 30)
+
+        query_expected = {
+            'name': {'operator': 'eq', 'value': 'felipe'},
+            'age': {'operator': 'eq', 'value': 30},
+            'agregate': 'and'
+        }
+
+        self.assertEqual(query_expected, query)
